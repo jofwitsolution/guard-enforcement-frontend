897c8bd3a15e64af25e51dac04c150f26dee9283
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2023-01-10 14:54:18 +0100

    section three of about page

--- src/pages/about/components/SectionThree.js
+++ src/pages/about/components/SectionThree.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-// import { BsFillCheckCircleFill } from 'react-icons/bs';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
 
 import styles from '../../../styles/tailwind';
 const SectionThree = () => {
@@ -22,7 +22,7 @@ const SectionThree = () => {
                 We are dedicated to keeping your people, properties and assets
                 through deep understanding and mitigation of security risks.
                 With our combined expertise and resource, we effectively deliver
-                security services and solutions in a wide array of inductries.
+                security services and solutions in a wide array of industries.
               </p>
             </motion.div>
           </div>
@@ -37,6 +37,103 @@ const SectionThree = () => {
             </motion.div>
           </div>
         </div>
+        <div className='flex flex-col md:flex-row items-start mt-4'>
+          <div className='flex-[40%]'>
+            <motion.div
+              whileInView={{ opacity: [0, 1], x: [-100, 0] }}
+              transition={{ duration: 1, type: 'tween' }}
+              viewport={{ once: true }}
+              className='bg-white py-5 px-10 shadow-xl'
+            >
+              <span className='inline-block font-bold mb-2 md:mb-3 text-[1.3rem]'>
+                Vision
+              </span>
+              <p>
+                To have a society whose security is guaranteed through our
+                security expertise.
+              </p>
+            </motion.div>
+          </div>
+          <div className='flex-[50%] self-end md:self-center flex items-center justify-center'>
+            <motion.div
+              whileInView={{ opacity: [0, 1], x: [100, 0] }}
+              transition={{ duration: 1, type: 'tween' }}
+              viewport={{ once: true }}
+              className='left-point bg-primaryAlt text-white flex items-center justify-center'
+            >
+              <span className='text-white z-50 text-[1.5rem] font-bold'>
+                Our Vision
+              </span>{' '}
+            </motion.div>
+          </div>
+        </div>
+        <div className='flex flex-col md:flex-row items-start mt-4'>
+          <div className='flex-[30%]'>
+            <motion.div
+              whileInView={{ opacity: [0, 1], x: [-100, 0] }}
+              transition={{ duration: 1, type: 'tween' }}
+              viewport={{ once: true }}
+              className='bg-white py-5 px-10 shadow-xl'
+            >
+              <span className='inline-block font-bold mb-2 md:mb-3 text-[1.3rem]'>
+                Mission
+              </span>
+              <p>To provide total security assurance to our client always!</p>
+            </motion.div>
+          </div>
+          <div className='flex-[50%] self-end md:self-center flex items-center justify-center'>
+            <motion.div
+              whileInView={{ opacity: [0, 1], x: [100, 0] }}
+              transition={{ duration: 1, type: 'tween' }}
+              viewport={{ once: true }}
+              className='left-arrow bg-primary text-white flex items-center justify-center'
+            >
+              <span className='text-white z-50 text-[1.5rem] font-bold'>
+                Our Mission
+              </span>{' '}
+            </motion.div>
+          </div>
+        </div>
+        <motion.div
+          whileInView={{ opacity: [0, 1], y: [100, 0] }}
+          transition={{ duration: 1, type: 'tween' }}
+          viewport={{ once: true }}
+          className='rabbet-clip mt-4 mx-auto bg-white shadow-xl w-[98%] xs:w-[70%] sm:w-[40%] flex flex-col justify-center items-center'
+        >
+          <span className='inline-block font-bold mb-2 md:mb-3 text-[1.3rem]'>
+            Innate value
+          </span>
+          <div className='flex gap-4 mb-2'>
+            <div>
+              <BsFillCheckCircleFill className='text-[1.6rem] text-primary' />
+            </div>
+            <p>Expertise</p>
+          </div>
+          <div className='flex gap-4 mb-2'>
+            <div>
+              <BsFillCheckCircleFill className='text-[1.6rem] text-primary' />
+            </div>
+            <p>Commitment</p>
+          </div>
+          <div className='flex gap-4 mb-2'>
+            <div>
+              <BsFillCheckCircleFill className='text-[1.6rem] text-primary' />
+            </div>
+            <p>Excellence</p>
+          </div>
+          <div className='flex gap-4 mb-2'>
+            <div>
+              <BsFillCheckCircleFill className='text-[1.6rem] text-primary' />
+            </div>
+            <p>Strong</p>
+          </div>
+          <div className='flex gap-4 mb-2'>
+            <div>
+              <BsFillCheckCircleFill className='text-[1.6rem] text-primary' />
+            </div>
+            <p>Personality</p>
+          </div>
+        </motion.div>
       </div>
     </section>
   );
