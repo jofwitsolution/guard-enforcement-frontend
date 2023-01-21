036891a312508bb6f8fbc9013bb99b50a5b5703b
Faleye Oluwafemi joseph <faleyeoluwafemi1@gmail.com>
2023-01-21 21:47:19 +0100

    hero text

--- src/pages/ge-nigeria/components/SectionOne.js
+++ src/pages/ge-nigeria/components/SectionOne.js
@@ -10,7 +10,8 @@ const SectionOne = () => {
             Guard Enforcement Nigeria
           </h1>
           <p className='text-gray-500 my-5 text-2xl'>
-            Your trusted partner for professional security solutions
+            Protection you can count on, Guard Enforcement Nigeria - Your
+            safeguard in an uncertain world
           </p>
         </div>
         <div className='mx-auto px-4 mt-16'>
@@ -20,10 +21,17 @@ const SectionOne = () => {
             Nigeria. We provide a wide range of security solutions to
             individuals and organizations, and our goal is to provide the same
             standard of security services as obtainable in the USA and other
-            parts of the developed world. Our services include security
-            consultation and implementation, surveillance and alarm system
-            installation, journey management, crowd control and event
-            management.
+            parts of the developed world. With our team of dedicated
+            professionals and cutting-edge technology, we are committed to
+            providing you with the highest level of security and protection.
+            From security consultation and implementation to surveillance and
+            alarm systems, we have the expertise and experience to meet all of
+            your security needs. Together, we can create a safer and more secure
+            environment for you, your family, and your organization. Trust us to
+            keep you safe, so you can focus on what matters most. Our services
+            include security consultation and implementation, surveillance and
+            alarm system installation, journey management, crowd control and
+            event management.
           </p>
         </div>
       </div>
